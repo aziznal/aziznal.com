19983bd79eb89edc70bcf4fe2f2360ad84b89f2c
aziznal <aziznal.dev@gmail.com>
2022-12-11 17:30:36 +0300

    feat: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="fw-bold mt-2">Hello World!</h1>
+            <h1 className="fw-bold mt-2">Hello World!</h1>
+            <h1 className="fw-bold mt-2">Hello World!</h1>
+            <h1 className="fw-bold mt-2">Hello World!</h1>
+        </>
+    );
+}
