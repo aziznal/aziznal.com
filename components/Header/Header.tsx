0afc401983ed930d2d65f97c4184ff74963de2e0
Aziz <aziznal.dev@gmail.com>
2023-04-21 21:47:07 +0300

    setup header styles with links

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,22 @@
+import styles from "./Header.module.scss";
+
 export function Header() {
     return (
-        <header className="d-flex fx-row fx-main-axis-center fx-cross-axis-center fx-gap-2">
-            <span>Link 1</span>
-            <span>Link 2</span>
-            <span>Link 3</span>
+        <header className={styles["header"]}>
+            <div className={styles["link-container"]}>
+                <span className={styles["link"]} data-active>
+                    Home
+                </span>
+                <span className={styles["link"]}>About</span>
+                <span className={styles["link"]}>Projects</span>
+                <span className={styles["link"]}>Articles</span>
+                <span className={styles["link"]}>Contact Me</span>
+            </div>
+
+            <div className={styles["link-container"]}>
+                <span>Lang</span>
+                <span>Theme</span>
+            </div>
         </header>
     );
 }
